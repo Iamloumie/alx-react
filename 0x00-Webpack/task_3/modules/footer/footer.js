9095d1ce64509cd0e71041ed06f221a800c15fa6
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-05-13 00:49:10 +0100

    fixed checker error

--- 0x00-Webpack/task_3/modules/footer/footer.js
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -2,4 +2,4 @@ const $ = require('jquery');
 import './footer.css';
 
 $('body').append('<footer></footer>');
-$('footer').append('<p>Copyright - Holberton School</p>');
+$('footer').append('<p>Copyright - ALX</p>');
